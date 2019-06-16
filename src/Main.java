2ed5726bd2997e58eb5e2914070bee7c07fedb07
AtaPieja <bpieja@poczta.fm>
2019-06-16 12:27:17 +0200

    local Message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String localMessage  ="Hello World";
+        print (localMessage);
 
+    }
+    private static void print (String message){
+        System.out.println( message);
     }
 }
